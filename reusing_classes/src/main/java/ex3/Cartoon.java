fb55efb8be6ff10eff16f1b54d64c5c308e1c783
Alexandru Tenche <alex.tenche@gmail.com>
2020-05-03 12:32:02 +0300

    added ex3

---
+++ reusing_classes/src/main/java/ex3/Cartoon.java
@@ -0,0 +1,8 @@
+package ex3;
+
+public class Cartoon extends Drawing {
+
+    public Cartoon() {
+        System.out.println("Cartoon constructor");
+    }
+}
---
+++ reusing_classes/src/test/java/ex3/CartoonTest.java
@@ -0,0 +1,14 @@
+package ex3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartoonTest {
+
+    @Test
+    void ConstructorOrder() {
+        Cartoon cartoon = new Cartoon();
+    }
+
+}
